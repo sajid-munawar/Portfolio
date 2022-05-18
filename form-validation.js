189dda4ed8d63d5c35705afce56bd0207b2744d3
Abby Nyakara <81410040+AbbyNyakara@users.noreply.github.com>
2022-05-18 10:39:55 +0300

    Add contact form validation
    - Check if the email is in lowercase and show an error if its not.

---
+++ form-validation.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const small = email.parentNode.querySelector('small');
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
+    e.preventDefault();
+    small.innerText = 'Form not sent.Please enter email in lower case';
+  } else if (email.value === email.value.toLowerCase()) small.innerText = '';
+});
+email.addEventListener('click', () => {
+  small.innerText = '';
+});
